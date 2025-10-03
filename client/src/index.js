9301b4277e7d331e9b5100d4116a079b24c2c190
JQuiej <quiej008@gmail.com>
2025-10-03 03:24:07 -0600

    subida BCK 7

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+// Ya no necesitas la siguiente línea, así que elimínala:
+// import { GoogleOAuthProvider } from '@react-oauth/google'; 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +9,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    {/* Simplemente renderiza tu componente App directamente */}
+    <App />
   </React.StrictMode>
 );
 
